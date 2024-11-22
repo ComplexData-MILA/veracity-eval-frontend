8f7a2dc032051e602c3f1f65cb352f2d250e2a39
Michael Pinder <mike.pinder@gmail.com>
2024-11-21 18:40:21 -0600

    added links to user menu

--- src/app/components/userModal/index.tsx
+++ src/app/components/userModal/index.tsx
@@ -1,4 +1,5 @@
 
+import Link from "next/link";
 import styles from "./userModal.module.scss";
 
 
@@ -13,17 +14,21 @@ const UserModal = ({ setAccountControlIsOpen }: Props) => {
   return (
       <div className={styles.tintedWrapper} onClick={()=> setAccountControlIsOpen(false)}>
         <div className={styles.menu}>
-          <ul>
-            <li>Account Management</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
+          <ul className={styles.linkList}>
+            <li>Preferences</li>
           </ul>
+          <ul className={styles.linkList}>
+            <Link href='/privacy'><li>Privacy Policy</li></Link>
+            <Link href='/privacy'><li>Terms of Service</li></Link>
+            <Link href='/privacy'><li>Compliance Policy</li></Link>
+            <Link href='/privacy'><li>User Guidelines</li></Link>
+          </ul>
+          <ul className={styles.linkListLast}>
+            <li>Logout</li>
+          </ul>
+          
           </div>
+          <div className={styles.littleTriangle} />
       </div>
   );
 }
