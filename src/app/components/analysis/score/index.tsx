39ff5099deced7d91dd88a40b8b62b5c75e8dce4
Michael Pinder <mike.pinder@gmail.com>
2024-12-03 13:15:03 -0500

    small additions to the sources component

--- src/app/components/analysis/score/index.tsx
+++ src/app/components/analysis/score/index.tsx
@@ -5,9 +5,22 @@ import styles from "../analysis.module.scss";
 export default function Score() {
 
   return (
-      <div className={styles.score}>
-        <h4 className={styles.heading}>Reliability score</h4>
-        <p className={styles.subheading}>Based on word analysis from verified sources.</p>
-      </div>
+      <section className={styles.score}>
+        <div className={styles.scoreHeader}>
+          <div>
+            <h4 className={styles.heading}>Reliability score</h4>
+            <p className={styles.subheading}>Based on word analysis from verified sources.</p>
+          </div>
+          <p className={styles.subheading}>How Veracity interpreted your prompt</p>
+        </div>
+        <div className={styles.scoreMain} >
+          <div className={styles.scoreGraph}>
+
+          </div>
+          <div className={styles.scoreText}>
+
+          </div>
+        </div>
+      </section>
   );
 }
--- src/app/components/analysis/sources/barGraph.tsx
+++ src/app/components/analysis/sources/barGraph.tsx
@@ -5,7 +5,13 @@ import styles from "../analysis.module.scss";
 export default function BarGraph() {
 
   return (
-      <div className={styles.barGraph}>
+      <div className={styles.sourceCredibilityRow}>
+        <h2 className={styles.valueHeadingOne}>25</h2>
+        <div>
+          <p className={styles.averageText}>Avg. Source Credibility</p>
+          <hr />
+        </div>
+        <h2>100%</h2>
       </div>
   );
 }
