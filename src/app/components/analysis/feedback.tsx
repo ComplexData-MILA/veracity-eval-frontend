0f2e53b01863dd56bb58321ec7e2da4058e18398
Michael Pinder <mike.pinder@gmail.com>
2024-12-11 19:36:38 -0600

    feedback drawer component partially completed

--- src/app/components/analysis/feedback.tsx
+++ src/app/components/analysis/feedback.tsx
@@ -9,13 +9,24 @@ type Props = {
 export default function Feedback({ setSourceWindow }: Props) {
 
   return (
-    <div className={styles.feedbackRow}>
-      <div className={styles.feedbackWrapper}>
-          <p>Give us feedback on this answer</p>
-          <Image className={styles.feedbackThumb} src="/assets/thumbUp.svg" alt="me" width="20" height="20" onClick={()=> alert("yay!")} />
-          <Image className={styles.feedbackThumb} src="/assets/thumbDown.svg" alt="me" width="20" height="20" onClick={()=> alert("boo!")} />
+    <section>
+      <div className={styles.feedbackRow}>
+        <div className={styles.feedbackWrapper}>
+            <p>Give us feedback on this answer</p>
+            <Image className={styles.feedbackThumb} src="/assets/thumbUp.svg" alt="me" width="20" height="20" onClick={()=> alert("yay!")} />
+            <Image className={styles.feedbackThumb} src="/assets/thumbDown.svg" alt="me" width="20" height="20" onClick={()=> alert("boo!")} />
+        </div>
+        <p className={styles.interpretedLink} onClick={()=> setSourceWindow(2)}>How Veracity interpreted your prompt</p>
       </div>
-      <p className={styles.interpretedLink} onClick={()=> setSourceWindow(2)}>How Veracity interpreted your prompt</p>
-    </div>
+      <div className={styles.feedbackDrawer}>
+        <h4 className={styles.feedbackDrawerHeading}>Why did you select this feedback?</h4>
+        <textarea className={styles.feedbackTextInput} placeholder="what was unsatisfying about this response?" />
+        <div className={styles.bottomRow}>
+          <p className={styles.feedbackDisclaimer}>Veracity my use account and system data to understand your feedback and improve our quality,You can read more into details in Privacy Policy and Terms of service.</p>
+          <button>Cancel</button>
+          <button>Submit</button>
+        </div>
+      </div>
+    </section>
   );
 }
