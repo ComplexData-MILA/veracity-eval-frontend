36c21c15cfb6250bc03030c0bb7a9d0ed32904f0
Michael Pinder <mike.pinder@gmail.com>
2025-01-29 15:53:46 -0600

    dashboard changes

--- src/app/components/searchCard/index.tsx
+++ src/app/components/searchCard/index.tsx
@@ -10,7 +10,7 @@ export default function SearchCard(props:Props) {
 
   return (
     <div className={styles.sourceCard}>
-      <p className={styles.sourceNumberLine}><span className={styles.sourceNumberBlock}>Source {props.number}</span></p>
+      <p className={styles.sourceNumberLine}><span className={styles.sourceNumberBlock}>Prompt {props.number}</span></p>
           <p className={styles.searchHeading}> SEARCH: </p>
           <p className={styles.searchContent}>{props.prompt}</p>
           <p className={styles.summaryHeading}>SUMMARY: </p>
--- src/app/dashboard/panels/totals.tsx
+++ src/app/dashboard/panels/totals.tsx
@@ -14,8 +14,19 @@ export default function TotalsTile() {
                 </div>
             </div>
             <div className={styles.bottomTile}>
-            <h2 className={styles.title}>Retrieved Sources</h2>
-            <p className={styles.subtitle}>Information obtained from a query</p>
+                <div className={styles.topRowBottomTile}>
+                    <div className={styles.lhs}>
+                        <h2 className={styles.title}>Retrieved Sources</h2>
+                        <p className={styles.subtitle}>Information obtained from a query</p>
+                    </div>
+                    <div className={styles.rhs}>
+                        <h2 className={styles.subtitle}>Total sources used in reliability score</h2>
+                        <h3 className={styles.giantNumber}>2350</h3>
+                    </div>
+                </div>
+                <p className={styles.note}>
+                    Since this table has to order, filter, expand etc I'd use a third party component here, save yourself a bunch of dev time. It's been done a million times and we probably don't need to reinvent the wheel.
+                </p>
             </div>
         </div>
 
