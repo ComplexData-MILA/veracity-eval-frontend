4e7851b6dac6096900ae3ccf88df8195faf023e8
Michael Pinder <mike.pinder@gmail.com>
2025-02-05 16:42:04 -0600

    loading animation added while claim is being processed

--- src/app/components/chatBubbles/chatIn/index.tsx
+++ src/app/components/chatBubbles/chatIn/index.tsx
@@ -13,7 +13,9 @@ export default function ChatIn(props: Props) {
       <div className={styles.chatRow}>
         <Image src={'/assets/logo.png'} alt='veri-fact logo' height={40} width={40} />
         <div className={styles.chatBubbleIn}>
+          {props.text === "..." ? <Image src={'/assets/loadingCropped.gif'} alt='veri-fact logo' height={20} width={20} /> : 
           <p>{props.text}</p>
+          }
         </div>
       </div>
   );
