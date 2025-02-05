828573757638fff24816f656f54c84fe26622388
Michael Pinder <mike.pinder@gmail.com>
2025-02-05 16:24:16 -0600

    dashboard UI update, timeframe and logos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import ReliabilityMatrix from "./panels/reliabilityMatrix";
 import ReliabilityOverTime from "./panels/reliabilityTime";
 import TotalsTile from "./panels/totals";
 import WordCloud from "./panels/wordCloud";
+import Image from "next/image";
 
 
 export default function Dashboard() {
@@ -15,14 +16,32 @@ export default function Dashboard() {
             <h1 className={styles.title}>Dashboard</h1>
             <p className={styles.subtitle}>Integrating data from somewhen in spacetime</p>
           </div>
-          <div className={styles.topRowRight}>
-            <label>Timeframe</label>
-            <select>
-              <option>All time</option>
-              </select>
-          </div>
+          
         </div>
         <section className={styles.tileGrid}>
+              <div className={styles.selectorRowLeft}>
+              <div>
+                <label className={styles.timeframeTitle}>Timeframe</label>
+                <p className={styles.timeframeSubtitle}>Set a timeframe to update the dashboard</p>
+              </div>
+              <select className={styles.timeframeSelector}>
+                <option>All time</option>
+                <option>1 month</option>
+                <option>etc</option>
+                <option>etc...</option>
+                </select>
+              </div>
+              <div className={styles.selectorRowRight}>
+                <div className={styles.logoRow}>
+                <p className={styles.logoText}>Powered by</p>
+                {/*Put more logos here instead of these divs */}
+                <div /><div />
+                {/*new high quality ones probably */}
+                <Image src="/assets/mila.png" alt="me" width="116" height="41" className={styles.mila}/>
+                <Image src="/assets/udem.png" alt="me" width="100" height="41" />
+                <Image src="/assets/mcgillBlack.png" alt="me" width="116" height="30" />
+                </div>
+            </div>
           <ClusterTile />
           <TotalsTile />
           <ReliabilityOverTime />
--- src/app/dashboard/panels/reliabilityMatrix.tsx
+++ src/app/dashboard/panels/reliabilityMatrix.tsx
@@ -2,45 +2,44 @@
 import styles from "../page.module.scss"
 
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip
-  } from 'chart.js';
-  import { Line } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
 
 
 ChartJS.register(
-CategoryScale,
-LinearScale,
-PointElement,
-LineElement,
-Tooltip
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip
 );
 
 export const options = {
 responsive: true,
 plugins: {
-    legend: {
-        display: false,
-        }
-    }
+  legend: {
+      display: false,
+      }
+  }
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+const labels = ['1 star', '2 star', '3 star', '4 star', '5 star'];
 
 export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Reliability over time',
-      data: labels.map(() => Math.random()*Math.random()*100),
-      borderColor: '#1683FF',
-      backgroundColor: '#11F90E',
-    },
-  ],
+labels,
+datasets: [
+  {
+    label: 'Reliability over time',
+    data: labels.map(() => Math.random()*Math.random()*100),
+    borderColor: '#1683FF',
+    backgroundColor: '#1683FF',
+  },
+],
 };
 
 export default function ReliabilityMatrix() {
@@ -50,7 +49,7 @@ export default function ReliabilityMatrix() {
             <div className={styles.chartWrapper} style={{marginTop: '0px'}}>
                 <h3 className={styles.subtitle}>Higher reliability score attained</h3>
                 <h4 className={styles.giantNumber}>50%</h4>
-                <Line options={options} data={data} />
+                <Bar options={options} data={data} />
             </div>
         </div>
 );
