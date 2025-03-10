370b1a35f1f01141e0dcf4674d6cc583c97e12a3
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-03-10 15:08:57 -0400

    Add the deployment api

--- src/app/dashboard/panels/wordCloud.tsx
+++ src/app/dashboard/panels/wordCloud.tsx
@@ -1,20 +1,3 @@
-// import styles from "../page.module.scss"
-
-
-// export default function WordCloud() {
-    
-//     return (
-//          <div className={styles.mockTile}>
-//             <h2 className={styles.title}>Claim Word cloud</h2>
-//             <div className={styles.chartWrapper}>
-//                 <h1>word</h1>
-//                 <h2>cloud</h2>
-//                 <p>goes</p>
-//                 <h3>here</h3>
-//             </div>
-//         </div>
-// );
-// }
 "use client"; 
 
 import React, { useEffect, useState } from "react";
