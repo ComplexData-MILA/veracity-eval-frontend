514adac58d0b72e69492f4a37d428808771f98e4
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-03-20 17:01:30 -0400

    input added

--- src/app/components/input/index.tsx
+++ src/app/components/input/index.tsx
@@ -3,7 +3,6 @@ import { useTranslations } from "next-intl";
 import styles from "./input.module.scss";
 import Image from 'next/image';
 import { useEffect, useState, useRef } from "react";
-import { useSearchParams } from "next/navigation";
 
 type Props = {
   setClaim: (arg0: string) => void;
@@ -14,7 +13,6 @@ type Props = {
 export default function Input({setClaim, verifyClaim, claim}: Props) {
   const t = useTranslations('chatpage');
   const [inputText, setInputText] = useState<string>("");
-  const searchParams = useSearchParams();
   const formRef = useRef<HTMLFormElement>(null);
 
   const handleChange = (newText: string) => {
@@ -26,19 +24,20 @@ export default function Input({setClaim, verifyClaim, claim}: Props) {
     setInputText("");
   }
   useEffect(() => {
-  const query = searchParams.get("q");
-  if (query) {
-    const decodedQuery = decodeURIComponent(query);
-    setInputText(decodedQuery);
+    // Access URL parameters using window.location.search
+    const queryParams = new URLSearchParams(window.location.search);
+    const q = queryParams.get("q");
 
-    // Ensure the form submission is triggered after the input is set
-    setTimeout(() => {
-      if (formRef.current) {
-        formRef.current.dispatchEvent(new Event("submit", { bubbles: true }));
-      }
-    }, 0);
-  }
-  }, [searchParams]);
+    if (q) {
+      const decodedQuery = decodeURIComponent(q);
+      setInputText(decodedQuery);
+
+      // Automatically trigger form submission
+      setTimeout(() => {
+        formRef.current?.dispatchEvent(new Event("submit", { bubbles: true }));
+      }, 100);
+    }
+  }, []);
   useEffect(() => {
     if (claim !== '') {
         verifyClaim();
