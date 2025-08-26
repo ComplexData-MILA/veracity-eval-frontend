d78e37841bed4c0d8f709b892aa15574bd94e158
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-08-26 16:55:13 -0400

    redirect reconfig

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
-  const returnTo = url.searchParams.get('returnTo') || '/chat';
+  // const returnTo = url.searchParams.get('returnTo') || '/chat';
 
   const origin = process.env.AUTH0_BASE_URL
   const auth0Domain = process.env.AUTH0_ISSUER_BASE_URL!;
@@ -11,10 +11,11 @@ export async function GET(req: NextRequest) {
   const signupUrl = `${auth0Domain}/authorize?` +
     new URLSearchParams({
       client_id: clientId,
-      redirect_uri: `${origin}/api/auth/callback${returnTo}`,
+      redirect_uri: `${origin}/api/auth/login`,
       response_type: 'code',
       scope: 'openid profile email',
       screen_hint: 'signup',
+      state: '/chat'
     }).toString();
 
   return NextResponse.redirect(signupUrl);
