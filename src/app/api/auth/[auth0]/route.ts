8dcc7a79b6f5ec17381f95a9e2534df92fba7983
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-08-26 11:29:57 -0400

    changed the auth

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,14 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const GET = handleAuth();
+export default handleAuth({
+  login: async (req: NextApiRequest, res: NextApiResponse) => {
+    const screenHint = req.query.screen_hint === 'signup' ? 'signup' : undefined;
+
+    await handleLogin(req, res, {
+      authorizationParams: {
+        screen_hint: screenHint,
+      },
+    });
+  },
+});
