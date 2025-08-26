bc051f4300d5c97a06fd5ea9776fb8ae8a014489
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-08-26 15:47:07 -0400

    add the route

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -4,14 +4,14 @@ export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const returnTo = url.searchParams.get('returnTo') || '/chat';
 
-//   const origin = process.env.NEXT_PUBLIC_API_URL
-  const auth0Domain = process.env.AUTH0_BASE_URL!;
+  const origin = process.env.AUTH0_BASE_URL
+  const auth0Domain = process.env.AUTH0_ISSUER_BASE_URL!;
   const clientId = process.env.AUTH0_CLIENT_ID!;
 
   const signupUrl = `${auth0Domain}/authorize?` +
     new URLSearchParams({
       client_id: clientId,
-      redirect_uri: `${auth0Domain}/api/auth/callback${returnTo}`,
+      redirect_uri: `${origin}/api/auth/callback${returnTo}`,
       response_type: 'code',
       scope: 'openid profile email',
       screen_hint: 'signup',
