d39473065315a0d53c14a9dec4312a9eb72d0fc6
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-09-04 15:39:37 -0400

    signup change

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getSession } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0/edge';
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
