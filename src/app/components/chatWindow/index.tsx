02875841a9a397f09a90c64235603cb443384bf6
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-09-09 13:56:00 -0400

    preserving whitespace

--- src/app/components/chatWindow/index.tsx
+++ src/app/components/chatWindow/index.tsx
@@ -113,6 +113,7 @@ export default function ChatWindow() {
       
       const analysisData = await analysisResponse.json();
       setFinalAnalysis(analysisData);
+      console.log(JSON.stringify(analysisData.analysis_text));
       await fetchSources(data.content.analysis_id);
       await fetchSearches(data.content.analysis_id);
     } catch (err) {
