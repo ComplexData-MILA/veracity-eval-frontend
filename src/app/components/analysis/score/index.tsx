072acc62f16a2fb27bd63b4fb1d101c96ba12227
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-10-10 16:52:18 -0400

    cleanup unused variables

--- src/app/components/analysis/score/index.tsx
+++ src/app/components/analysis/score/index.tsx
@@ -7,10 +7,9 @@ import { useTranslations } from "next-intl";
 type Props = {
   veracityScore: number;
   text: string;
-  setActiveModal: (arg0: number) => void;
 };
 
-export default function Score({veracityScore, text, setActiveModal}: Props) {
+export default function Score({veracityScore, text}: Props) {
   const t = useTranslations('chatpage');
   const reliability:number = veracityScore*100
 
