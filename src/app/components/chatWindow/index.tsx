f2cd8ed8bd5f6aa3bc07404f35d3848f59481a9a
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-11-13 15:16:04 -0500

    no score phase 2

--- src/app/components/chatWindow/index.tsx
+++ src/app/components/chatWindow/index.tsx
@@ -149,7 +149,7 @@ export default function ChatWindow() {
           claim_text: claim,
           context: claim,
           language: language,
-          batch_post_id: "group_one"
+          batch_post_id: "control_part_two"
         })
       });
   
