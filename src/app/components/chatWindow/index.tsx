fea7a1fa6b0094723b0ee824ca89268ee482ba67
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-11-13 15:26:26 -0500

    treatment label

--- src/app/components/chatWindow/index.tsx
+++ src/app/components/chatWindow/index.tsx
@@ -149,7 +149,7 @@ export default function ChatWindow() {
           claim_text: claim,
           context: claim,
           language: language,
-          batch_post_id: "group_one"
+          batch_post_id: "exp_one_treatment_part_two"
         })
       });
   
