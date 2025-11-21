73d31c584e790a0fe4177716666ceb2e17fecad1
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-11-21 18:39:03 -0500

    extra space

--- src/app/components/analysis/score/index.tsx
+++ src/app/components/analysis/score/index.tsx
@@ -20,6 +20,7 @@ export default function Score({text}: Props) {
                 <React.Fragment key={index}>
                   {line}
                   <br />
+                  <br />
                 </React.Fragment>
               ))}
             </p>
